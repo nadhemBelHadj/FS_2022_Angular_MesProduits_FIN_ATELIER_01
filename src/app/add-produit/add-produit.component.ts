08aad3a6deccbcbff4ff4312a7056cc9112a41cb
unknown <nadhemb@yahoo.com>
2022-05-04 16:18:08 +0100

    first commit

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html'
+})
+export class AddProduitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
